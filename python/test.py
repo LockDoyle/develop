88d90330d058bac226d2d8e4ff2d752130d345dc
Lock Doyle <lockdoyle.tech@gmail.com>
2021-09-29 09:16:00 +0900

    python add

--- python/test.py
+++ python/test.py
@@ -1,8 +1,21 @@
-# math
-a:int
-b:int
 
-a = 2
-b = 3
+#self ⇒ instance自身
+class test():
+	def __init__(self):
+		self.strA = 'test'
+		
+	def main(self):
+		self.math()
 
-print(a * b)
+	def math(self):
+		a:int
+		b:int
+
+		a = 2
+		b = 3
+
+		print(a * b)
+		print(self.strA)
+		
+t=test() # ()がないと、暗黙的にselfが引数に設定されない test()が正解
+t.main()
