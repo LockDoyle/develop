58ab5a42962f44d862dcfb912b45cc8e4cd759ac
Lock Doyle <lockdoyle.tech@gmail.com>
2021-10-08 19:52:56 +0900

    numpy

--- python/test_numpy.py
+++ python/test_numpy.py
@@ -7,15 +7,21 @@ print(tech)
 #二次元配列　配列同士の要素数は同じにしないといけないらしい。
 tech2=np.array([[1,2,3],[4,5,6]])
 print(tech2)
+print()
 
+print("■次元変換(ｶｯｺｲｲ")
+tech3_1=np.array([1,2,3,4,5,6,7,8])
+tech3_2=tech3_1.reshape(2,4)
+print(tech3_1)
+print(tech3_2)
 
 print(np.array([-1,0,1], dtype=np.float64))
 
 
 a=np.array([-1,0,1])
 
-
-print("配列のデータ属性")
+print()
+print("■配列のデータ属性")
 print(a.dtype)
 print(a.shape)
 print(a.ndim)
@@ -24,5 +30,5 @@ print(a.flat)
 print(a.T)
 
 print("")
-print("構築関数")
+print("■構築関数")
 print(np.arange(0,10,2))
